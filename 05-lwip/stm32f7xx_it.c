835214367a01d02a14286bb6d7541876a953ad5d
xtrinch <mojca.rojko@gmail.com>
2018-01-04 12:35:36 +0100

    LWIP compiles! Yay!

--- 05-lwip/stm32f7xx_it.c
+++ 05-lwip/stm32f7xx_it.c
@@ -123,15 +123,6 @@ void UsageFault_Handler(void)
 }
 
 /**
-  * @brief  This function handles SVCall exception.
-  * @param  None
-  * @retval None
-  */
-void SVC_Handler(void)
-{
-}
-
-/**
   * @brief  This function handles Debug Monitor exception.
   * @param  None
   * @retval None
@@ -140,14 +131,6 @@ void DebugMon_Handler(void)
 {
 }
 
-/**
-  * @brief  This function handles PendSVC exception.
-  * @param  None
-  * @retval None
-  */
-void PendSV_Handler(void)
-{
-}
 
 /**
   * @brief  This function handles SysTick Handler.
